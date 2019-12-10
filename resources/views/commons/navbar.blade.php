d0e252f30fa4235cbbaa6758047cee103f8a9d82
kent1121 <kent1121.sub@gmail.com>
2019-12-10 05:40:43 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,15 +1,27 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
     <a class="navbar-brand" href="/">Surprise Me!</a>
     
-     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar">
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar">
          <span class="navbar-toggler-icon"></span>
-     </button>
+    </button>
      
-     <div class="collapse navbar-collapse" id="navbar">
-         <ul class="navbar-nav mr-auto"></ul>
-         <ul class="navbar-nav">
-             <li class="nav-item">{!! link_to_route('signup.get', '新規ユーザー登録', [], ['class' => 'nav-link']) !!}</li>
-             <li class="nav-item"><a class="nav-link" href="#">ログイン</a></li>
-         </ul>
-     </div>
+    <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav mr-auto"></ul>
+        <ul class="navbar-nav">
+            @if (Auth::check())
+                <li class="nav-item"><a class="nav-link" href="#">ユーザー一覧</a></li>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li class="dropdown-item"><a href="#">My profile</a></li>
+                        <li class="dropdown-divider"></li>
+                        <li class="dropdown-item">{!! link_to_route('logout', 'ログアウト') !!}</li>
+                    </ul>
+                </li>
+            @else
+                <li class="nav-item">{!! link_to_route('signup.get', '新規ユーザー登録', [], ['class' => 'nav-link']) !!}</li>
+                <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
+            @endif
+        </ul>
+    </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the "Surprise Me!"</h1>
-            {!! link_to_route('signup.get', '新規ユーザー登録はこちら', [], ['class' => 'btn btn-lg btn-primary']) !!}
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the "Surprise Me!"</h1>
+                {!! link_to_route('signup.get', '新規ユーザー登録はこちら', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
